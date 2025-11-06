b7ce827a2fef6f277803000eb31391f42729c7db
mrtnrocks <contact@stackless.ventures>
2025-11-06 13:07:35 +0100

    27:00 min

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,11 @@
+import { RegisterForm } from "@/app/features/auth/components/register-form";
+
+const Page = () => {
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default Page;
--- src/generated/prisma/client.ts
+++ src/generated/prisma/client.ts
@@ -49,7 +49,17 @@ path.join(process.cwd(), "src/generated/prisma/query_engine-windows.dll.node")
  */
 export type User = Prisma.UserModel
 /**
- * Model Post
+ * Model Session
  * 
  */
-export type Post = Prisma.PostModel
+export type Session = Prisma.SessionModel
+/**
+ * Model Account
+ * 
+ */
+export type Account = Prisma.AccountModel
+/**
+ * Model Verification
+ * 
+ */
+export type Verification = Prisma.VerificationModel
