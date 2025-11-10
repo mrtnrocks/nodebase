df389e426953af8a1998f01da5ad61b859b47454
mrtnrocks <contact@stackless.ventures>
2025-11-10 13:00:43 +0100

    4 auth finished

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,6 +1,9 @@
 import { RegisterForm } from "@/app/features/auth/components/register-form";
+import { requireUnauth } from "@/lib/auth-utils";
 
-const Page = () => {
+const Page = async () => {
+  await requireUnauth();
+
   return (
     <div>
       <RegisterForm />
